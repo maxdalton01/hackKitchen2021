0b30c1d5c5ff485a4d092d9768bcaeaeaa08ba5c
Maxwell Dalton <maxdalton6@gmail.com>
2021-05-08 12:06:45 -0700

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-// Hack Kitechn 2021
+// Hack Kitchen 2021
 
 // include statements
 #include <iostream>
 #include <string>
 
 using namespace std; 
+
+// .................
+// _________________
+// |||||||||||||||||
 
 int isRaw(int time) {
     if (time < 8 && time > 0)
@@ -17,6 +21,10 @@ int isRaw(int time) {
         return 1; // kudos to you, time was right
 }
 
+// .................
+// _________________
+// |||||||||||||||||
+
 bool isIdiotSandwich (string str) {
     for (int i = 0; i < str.length(); i++) {
         str[i] = tolower(str[i]);
@@ -26,29 +34,34 @@ bool isIdiotSandwich (string str) {
     return false;
 }
 
+// .................
+// _________________
+// |||||||||||||||||
+
+
 int main()
 {
     
     // function for determining raw or not
     // need to have it boiled for exactly 8 hours: gordon is tough!
 
-    double userTime;
-    string userName;
+    double userTime_s__p__hot_;
+    string userName_s__p__hot_;
     cout << "Welcome chef! What would you like to be called? " << endl;
-    getline(cin, userName);
-    userName = "Chef " + userName;
-    cout << "Nice to meet you " + userName + "." << endl;
+    getline(cin, userName_s__p__hot_);
+    userName_s__p__hot_ = "Chef " + userName_s__p__hot_;
+    cout << "Nice to meet you " + userName_s__p__hot_ + "." << endl;
     cout << "Today we will be cooking up an Irish specialty: Corned-Beef Hash." << endl;
-    cout << "The beef has been marinating for several days, you just need to stew it." << endl;
+    cout << "The meat has been marinating for several days, you just need to stew it." << endl;
     cout << "To do this, we will be using a slow-cooker." << endl;
     cout << "How long should we stew the beef in this slow-cooker for, chef?" << endl;
-    cin >> userTime;
+    cin >> userTime_s__p__hot_;
     
     cin.ignore(10000, '\n');
     
-    int raw = isRaw(userTime);
-    string idiotSandwich;
-    switch(raw) {
+    int raw_s__p__hot_ = isRaw(userTime_s__p__hot_);
+    string idiotSandwich_s__p__hot_;
+    switch(raw_s__p__hot_) {
         case 0:
             cout << "It's RAW! Get out!" << endl;
             return 1;
@@ -58,8 +71,8 @@ int main()
             break;
         case 2:
             cout << "What are you?" << endl;
-            getline(cin, idiotSandwich);
-            if(isIdiotSandwich(idiotSandwich)) {
+            getline(cin, idiotSandwich_s__p__hot_);
+            if(isIdiotSandwich(idiotSandwich_s__p__hot_)) {
                 cout << "Yes you are! Get out!" << endl;
                 return 1;
             }
@@ -69,5 +82,7 @@ int main()
             cout << "It's BURNT! Get out!" << endl;
             return 1;
     }
+    
+    cout << "You made it this far, you get a GOLD STAR" << endl;
     
 }
